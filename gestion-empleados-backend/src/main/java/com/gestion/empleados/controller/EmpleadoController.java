a827466823cd1e9b24ffc1f7478ddd718f9537b5
juan <juanprogrammer10@hotmail.com>
2025-02-28 19:46:29 -0500

    are algunos cambios

--- gestion-empleados-backend/src/main/java/com/gestion/empleados/controller/EmpleadoController.java
+++ gestion-empleados-backend/src/main/java/com/gestion/empleados/controller/EmpleadoController.java
@@ -3,6 +3,7 @@ package com.gestion.empleados.controller;
 import com.gestion.empleados.model.Empleado;
 import com.gestion.empleados.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoController {
     @Autowired
     private EmpleadoRepository repository;
